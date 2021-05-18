8cc3a8aa7cc1f1fe0c737f6563ef7de7024cb929
pharra <typechasing@gmail.com>
2021-05-18 16:22:09 +0800

    add main layout

--- 开发者挑战项目/共享影院/share-cinema/src/router/index.js
+++ 开发者挑战项目/共享影院/share-cinema/src/router/index.js
@@ -1,27 +1,29 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import routes from './routes'
-import Join from '../views/join/index.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import routes from "./routes";
+import Main from "../views/main/index.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-    mode: 'hash',
-    base: process.env.BASE_URL,
-    routes: [{
-      path: '/',
-      name: 'join',
-      component: Join
-    }].concat(routes)
-})
+  mode: "hash",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "main",
+      component: Main,
+    },
+  ].concat(routes),
+});
 
 router.onError((error) => {
   const pattern = /Loading chunk (\w)+ failed/g;
   const isChunkLoadFailed = error.message.match(pattern);
   const targetPath = router.history.pending.fullPath;
   if (isChunkLoadFailed) {
-     router.replace(targetPath);
+    router.replace(targetPath);
   }
-})
+});
 
-export default router
+export default router;
---
+++ 开发者挑战项目/共享影院/share-cinema/src/views/main/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <el-container>
+    <el-header></el-header>
+    <el-main>
+      <div class="carousel">
+        <el-carousel indicator-position="outside" type="card">
+          <el-carousel-item v-for="item in 4" :key="item">
+            <h3>{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+      <div class="container">
+        <div v-for="item in 12" :key="item + 18" class="content">
+          {{ item }}
+        </div>
+      </div>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "main",
+  data() {
+    return {
+      carouselData: [],
+      containerData: [],
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.el-main {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.carousel {
+  display: block;
+  width: 80%;
+
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+  }
+}
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  width: 80%;
+  justify-content: space-around;
+
+  .content {
+    background-color: #99a9bf;
+    width: 25%;
+    height: auto;
+    margin: 20px;
+  }
+}
+</style>
