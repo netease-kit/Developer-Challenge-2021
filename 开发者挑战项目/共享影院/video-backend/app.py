8399808ec3abf2e4fb7a4852f6ca6bfc7765cf19
pharra <typechasing@gmail.com>
2021-05-22 22:41:38 +0800

    add ssl

--- 开发者挑战项目/共享影院/video-backend/app.py
+++ 开发者挑战项目/共享影院/video-backend/app.py
@@ -166,4 +166,5 @@ def test_disconnect():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=5000, debug=False)
+    socketio.run(app, host='127.0.0.1', port=5000, debug=False,
+                 certfile="cert/cert.pem", keyfile="cert/key.pem")
